9967a27f85647f175a95fafa6be108f365602ea8
dpark4 <dparklolkong@gmail.com>
2024-10-21 21:54:57 -0500

    spring boot websocket setup wip

---
+++ wordhunt/src/main/java/com/example/wordhunt/WordhuntApplication.java
@@ -0,0 +1,13 @@
+package com.example.wordhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordhuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordhuntApplication.class, args);
+	}
+
+}
---
+++ wordhunt/src/test/java/com/example/wordhunt/WordhuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordhunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordhuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
