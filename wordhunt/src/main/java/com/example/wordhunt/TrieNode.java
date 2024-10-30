c4b16ed381e2ac3fd60be883b0fb6e5f89c57b99
dpark4 <dparklolkong@gmail.com>
2024-10-29 22:54:05 -0500

    trie and trienode

--- wordhunt/src/main/java/com/example/wordhunt/TrieNode.java
+++ wordhunt/src/main/java/com/example/wordhunt/TrieNode.java
@@ -6,6 +6,10 @@ import java.util.Map;
 public class TrieNode {
     private Map<Character, TrieNode> children = new HashMap<>();  
     private boolean isWord;
+    
+    public Map<Character, TrieNode> getChildren() {
+      return children;
+    }
 
     public boolean isWord() {
       return isWord;
